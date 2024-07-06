fee4743218d43db101f032e48378b50a7a30aed7
taisei08 <829taisei829@gmail.com>
2024-07-06 17:13:57 +0900

    feature:insertテスト

---
+++ reco_blo/app/api/user.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+const { cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const admin = require('firebase-admin');
+const serviceAccount = require('../../firebase-test-serviceAccount.json'); // 秘密鍵を取得
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const COLLECTION_NAME = 'users';
+  //　初期化する
+  if (admin.apps.length === 0) {
+    admin.initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+  const db = getFirestore();
+
+  if (req.method === 'POST') {
+    const docRef = db.collection(COLLECTION_NAME).doc();
+    const insertData = {
+      id: '1',
+      name: 'Symfo',
+    };
+    docRef.set(insertData);
+  }
+  res.status(200);
+}
