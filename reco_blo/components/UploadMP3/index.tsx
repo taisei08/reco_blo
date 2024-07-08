e2b5f1fcfe1bd8686fc731052016d66d87967e3c
Dai-xx <118973573+Dai-xx@users.noreply.github.com>
2024-07-08 11:48:43 +0900

    Fix: アップロード時のUIを追加

--- reco_blo/components/UploadMP3/index.tsx
+++ reco_blo/components/UploadMP3/index.tsx
@@ -1,19 +1,20 @@
 "use client";
-import { Progress } from "@radix-ui/themes";
+
 import React, { useState } from "react";
 import { RiUploadLine } from "react-icons/ri";
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { collection, doc, setDoc } from "firebase/firestore";
+import { Progress } from "@radix-ui/themes";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { storage } from "@/lib/firebase";
 import { db } from "@/lib/firebase";
-
+import "@radix-ui/themes/styles.css";
 
 export const UploadMP3 = (uid: any) => {
-  const [file, setFile]:any = useState(null);
+  const [file, setFile]: any = useState(null);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [downloadURL, setDownloadURL] = useState("");
 
-  const handleFileChange = (e:any) => {
+  const handleFileChange = (e: any) => {
     if (e.target.files[0]) {
       setFile(e.target.files[0]);
     }
@@ -45,10 +46,10 @@ export const UploadMP3 = (uid: any) => {
           console.log("File available at", downloadURL);
           // ドキュメントを挿入する関数
           const insertDocument = async () => {
-            const COLLECTION_NAME = 'posts';
+            const COLLECTION_NAME = "posts";
             const insertData = {
-              title: '1',
-              description: 'あいうえお',
+              title: "1",
+              description: "あいうえお",
               audio_url: downloadURL,
               created_at: new Date(),
               user_id: uid,
@@ -57,10 +58,10 @@ export const UploadMP3 = (uid: any) => {
             try {
               const docRef = doc(collection(db, COLLECTION_NAME));
               await setDoc(docRef, insertData);
-              return { message: 'success' };
-            } catch (error:any) {
-              console.error('Error adding document: ', error);
-              return { message: 'error', error: error.message };
+              return { message: "success" };
+            } catch (error: any) {
+              console.error("Error adding document: ", error);
+              return { message: "error", error: error.message };
             }
           };
 
@@ -75,16 +76,18 @@ export const UploadMP3 = (uid: any) => {
 
   return (
     <div>
-      <input type="file" accept="audio/mp3" onChange={handleFileChange} />
-      <button onClick={handleUpload}>
-        <RiUploadLine
-          size={30}
-          className="hover:text-red-500 transition duration-300"
-        />
-      </button>
-      <Progress value={uploadProgress} size="1" />
-      <div>Upload Progress: {uploadProgress}%</div>
-      {downloadURL && <a href={downloadURL}>Download MP3</a>}
+      <div className="flex items-center">
+        <input type="file" accept="audio/mp3" onChange={handleFileChange} />
+        <button onClick={handleUpload}>
+          <RiUploadLine
+            size={30}
+            className="hover:text-red-500 transition duration-300"
+          />
+        </button>
+      </div>
+      <div className="mt-2">
+        <Progress value={uploadProgress} />
+      </div>
     </div>
   );
 };
